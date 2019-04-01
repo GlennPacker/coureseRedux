2259e4dfcd647fe1033482ae37faab2839a119ed
glennpacker <glenn.packer@branthill.co.uk>
2019-04-01 16:49:53 +0100

    courses basic api

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,18 @@
 import * as actionTypes from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: actionTypes.CREATE_COURSE, course };
+}
+
+export function loadCoursesSucess (courses) {
+  return { type: actionTypes.LOAD_COURSES_SUCCESS, courses }
+}
+
+export function loadCourses () {
+  return function (dispatch) {
+    return courseApi.getCourses().then(courses => {
+      dispatch(loadCoursesSucess(courses));
+    })
+  }
 }
